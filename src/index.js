a1d03b806b76ca6291ff1aa3c6da0afd5cbf3613
mmilox23 <165060819+mmilox23@users.noreply.github.com>
2024-05-19 01:50:46 +0200

    added api intergration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+
+function updateWeather(response){
+    let temperatureElement = document.querySelector(".current-temperature-value");
+    let temperature = response.data.temperature.current;
+     let cityElement = document.querySelector("#current-city");
+
+     
+     cityElement.innerHTML = response.data.city;
+     temperatureElement.innerHTML = Math.round(temperature);
+}
+function search(city){
+  let apiKey = "c5fo45504b77t2a0d3178aceb4685e4f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchCity(event){
     event.preventDefault();
     let searchInputElement = document.querySelector(".search-input");
-    let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = searchInputElement.value;
+   
+    search(searchInputElement.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchCity);
+
+search("Pretoria")
