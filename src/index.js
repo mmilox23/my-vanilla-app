b9aa8b118b57fc071be3412662c2aecd6604971d
mmilox23 <165060819+mmilox23@users.noreply.github.com>
2024-05-19 00:40:39 +0200

    added js search engine

---
+++ src/index.js
@@ -0,0 +1,8 @@
+function searchCity(event){
+    event.preventDefault();
+    let searchInputElement = document.querySelector(".search-input");
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = searchInputElement.value;
+}
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", searchCity);
