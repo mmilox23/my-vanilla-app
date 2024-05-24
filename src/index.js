b7075ac6056f9ac9825e9103523abbc547191893
mmilox23 <165060819+mmilox23@users.noreply.github.com>
2024-05-24 16:08:18 +0200

    added api forecast intergration

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function updateWeather(response){
      timeElement.innerHTML = formatDate(date);
      temperatureElement.innerHTML = Math.round(temperature);
 
+  getForecast(response.data.city);  
+
 }
 function formatDate(date) {
     let minutes = date.getMinutes();
@@ -54,27 +56,44 @@ function searchCity(event){
    
     search(searchInputElement.value);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-function displayForecast() {
-   let days = ["Tue", "Wed", "Thu", "Fri","Sat"];
-   let forecastHtml = "";
+  return days[date.getDay()];
+}
 
-   days.forEach(function(day) {
+function getForecast(city){
+  let apiKey = "c5fo45504b77t2a0d3178aceb4685e4f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let forecastHtml = "";
+
+   response.data.daily.forEach(function(day, index) {
+    if (index < 5) {
        forecastHtml =
           forecastHtml +
    `
     <div class ="weather-forecast-day">
-         <div class ="weather-forecast-date">Tue</div>
-         <div class ="temperature-icon">🌤</div>
+         <div class ="weather-forecast-date">${formatDay(day.time)}</div>
+         <img src="${day.condition.icon_url}" class ="weather-forecast-temperature-icon"/>
         <div class ="weather-forecast-temperatures">
            <div class ="weather-forecast-temperature">
-             <strong>15°</strong>
+             <strong>${Math.round(day.temperature.maximum)}°</strong>
            
-         <span class ="weather-forecast-temperature-min">9°</span>
+         <span class ="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</span>
         </div>
     </div>
     </div>
-   ` ;});
+   ` ;
+    }
+   }
+);
 
 let forecastElement = document.querySelector("#forecast");
 forecastElement.innerHTML = forecastHtml;
@@ -83,5 +102,4 @@ let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", searchCity);
 
 search("Pretoria");
-displayForecast();
 
